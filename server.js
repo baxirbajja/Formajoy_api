8964054e3f7533c8ac792c5cc9e381f2528f717d
bachirbajja <medbachirbajja@gmail.com>
2025-05-07 23:09:40 +0100

    w

--- server.js
+++ server.js
@@ -24,10 +24,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? '*' : ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: process.env.NODE_ENV === 'production' ? ['https://formajoy.vercel.app', 'https://formajoy-git-main-formajoy.vercel.app'] : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  maxAge: 600
 }));
 app.use(express.json());
 
