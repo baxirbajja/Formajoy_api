ec3b4b7d4a30d49eb935bfe59e31647c699b2fec
bachirbajja <medbachirbajja@gmail.com>
2025-05-07 22:54:55 +0100

    ff

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: process.env.NODE_ENV === 'production' ? '*' : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
